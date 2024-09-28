c4b55d62d4e00fd52bcc267d3f6270ce61f8231d
Seonu-Jeong <sw.jeong97@gmail.com>
2024-09-28 18:57:00 +0900

    [level 0] Title: 홀짝 구분하기, Time: 152.44 ms, Memory: 63.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        String answer = "";
+        
+        if(n%2==0)
+            answer = " is even";
+        else
+            answer = " is odd";
+        
+        System.out.println(n+answer);
+    }
+}
