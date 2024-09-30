1ae61f05dd567593de6579c1753ac65661a61ac0
Seonu-Jeong <sw.jeong97@gmail.com>
2024-09-30 11:13:40 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int x = (total - (num*(num-1))/2)/num;
+        
+        for(int i=0;i<num;i++){
+            answer[i]=x+i;
+        }
+        return answer;
+    }
+}
