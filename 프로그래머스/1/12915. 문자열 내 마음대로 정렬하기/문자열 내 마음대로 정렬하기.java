e93e6f5b51170bf10896207fcbf88ca829722794
Seonu-Jeong <sw.jeong97@gmail.com>
2024-10-11 09:18:03 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.27 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        
+        Arrays.sort(strings, 
+            (String a, String b) -> {
+                if(a.charAt(n)==b.charAt(n))
+                    return a.compareTo(b);
+                
+                return a.charAt(n)-b.charAt(n);
+            }
+        );
+        return strings;
+    }
+}
