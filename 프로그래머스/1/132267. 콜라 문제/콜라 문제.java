3dafc76b6232faa3d63479fa9ea47a4522d1b988
Seonu-Jeong <sw.jeong97@gmail.com>
2024-10-16 09:15:11 +0900

    [level 1] Title: 콜라 문제, Time: 0.51 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,14 @@
+class Solution {
+    
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        if(n<a) return 0;
+        
+        if(n%a==0)
+            answer = solution(a, b, (n/a)*b) + (n/a)*b;
+        else
+            answer = solution(a, b, (n/a)*b+(n%a)) + (n/a)*b;
+        return answer;
+    }
+}
