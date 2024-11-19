0e8b10640b659d49a1ba92209cf8c27e6a1f6211
Seonu-Jeong <sw.jeong97@gmail.com>
2024-11-19 20:51:18 +0900

    [level 0] Title: 더 크게 합치기, Time: 2.14 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b) {
+        int ab = Integer.parseInt(String.valueOf(a)+String.valueOf(b));
+        int ba = Integer.parseInt(String.valueOf(b)+String.valueOf(a));
+        
+        return ab<ba?ba:ab;
+    
+    }
+}
