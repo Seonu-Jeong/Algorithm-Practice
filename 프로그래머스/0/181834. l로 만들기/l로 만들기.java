30eb4955891a541b3214378e605d3c3923f726f4
Seonu-Jeong <sw.jeong97@gmail.com>
2025-04-17 23:16:58 +0900

    [level 0] Title: l로 만들기, Time: 9.78 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder answer = new StringBuilder();
+        
+        for(char ch : myString.toCharArray()){
+            if(ch<'l')
+                answer.append('l');
+            else
+                answer.append(ch);
+        }
+        return answer.toString();
+    }
+}
